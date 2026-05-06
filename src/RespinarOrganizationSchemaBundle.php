a5a107dd55028b1d4df1f34012c268a7cfd8302f
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-06 09:15:56 +0330

    Add bundle class with service configuration loading

---
+++ src/RespinarOrganizationSchemaBundle.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Organization Bundle.
+ *
+ * (c) Hamid Peywasti
+ *
+ * @license MIT
+ */
+
+namespace Respinar\OrganizationSchemaBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class RespinarOrganizationSchemaBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void
+    {
+        $containerConfigurator->import('../config/services.yaml');
+    }
+}
