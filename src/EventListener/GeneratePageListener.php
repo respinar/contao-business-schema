459c79395018ef3b3bfd5bb84078c774b2a3b654
Hamid Peywasti <hamid.peywasti@gmail.com>
2026-05-08 11:11:57 +0330

    Rename bundle and update field names to business schema

--- src/EventListener/GeneratePageListener.php
+++ src/EventListener/GeneratePageListener.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of Contao Organization Bundle.
+ * This file is part of Contao Business Bundle.
  *
  * (c) Hamid Peywasti
  *
@@ -10,7 +10,7 @@
 
 declare(strict_types=1);
 
-namespace Respinar\OrganizationSchemaBundle\EventListener;
+namespace Respinar\BusinessSchemaBundle\EventListener;
 
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\CoreBundle\Routing\ResponseContext\JsonLd\JsonLdManager;
@@ -33,7 +33,7 @@ class GeneratePageListener
     {
         $rootPage = PageModel::findById($pageModel->rootId);
 
-        if (null === $rootPage || empty($rootPage->orgschema_name)) {
+        if (null === $rootPage || empty($rootPage->business_name)) {
             return;
         }
 
@@ -47,20 +47,21 @@ class GeneratePageListener
         $graph = $jsonLdManager->getGraphForSchema(JsonLdManager::SCHEMA_ORG);
         $id = Environment::get('url').'/#organization';
 
-        $org = match ($rootPage->orgschema_type) {
+        $org = match ($rootPage->business_type) {
             'local_business' => $graph->localBusiness($id),
             default => $graph->organization($id),
         };
-        $org->name($rootPage->orgschema_name);
+        $org->identifier($id);
+        $org->name($rootPage->business_name);
 
-        if (!empty($rootPage->orgschema_legal_name)) {
-            $org->legalName($rootPage->orgschema_legal_name);
+        if (!empty($rootPage->business_legal_name)) {
+            $org->legalName($rootPage->business_legal_name);
         }
 
-        if (!empty($rootPage->orgschema_alternate_name)) {
+        if (!empty($rootPage->business_alternate_name)) {
             $alternateNames = array_values(array_filter(array_map(
                 static fn (string $name): string => trim($name),
-                explode(',', $rootPage->orgschema_alternate_name),
+                explode(',', $rootPage->business_alternate_name),
             )));
 
             if ([] !== $alternateNames) {
@@ -68,70 +69,70 @@ class GeneratePageListener
             }
         }
 
-        if (!empty($rootPage->orgschema_description)) {
-            $org->description($rootPage->orgschema_description);
+        if (!empty($rootPage->business_description)) {
+            $org->description($rootPage->business_description);
         }
 
-        if (!empty($rootPage->orgschema_url)) {
-            $org->url($rootPage->orgschema_url);
+        if (!empty($rootPage->business_url)) {
+            $org->url($rootPage->business_url);
         }
 
-        if (!empty($rootPage->orgschema_logo)) {
-            $logoUrl = $this->resolveFileUrl($rootPage->orgschema_logo);
+        if (!empty($rootPage->business_logo)) {
+            $logoUrl = $this->resolveFileUrl($rootPage->business_logo);
 
             if (null !== $logoUrl) {
                 $org->logo($logoUrl);
             }
         }
 
-        if (!empty($rootPage->orgschema_telephone) || !empty($rootPage->orgschema_email)) {
+        if (!empty($rootPage->business_telephone) || !empty($rootPage->business_email)) {
             $contactPoint = new ContactPoint();
 
-            if (!empty($rootPage->orgschema_telephone)) {
-                $contactPoint->telephone($rootPage->orgschema_telephone);
+            if (!empty($rootPage->business_telephone)) {
+                $contactPoint->telephone($rootPage->business_telephone);
             }
 
-            if (!empty($rootPage->orgschema_email)) {
-                $contactPoint->email($rootPage->orgschema_email);
+            if (!empty($rootPage->business_email)) {
+                $contactPoint->email($rootPage->business_email);
             }
 
             $org->contactPoint($contactPoint);
         }
 
         if (
-            !empty($rootPage->orgschema_street_address)
-            || !empty($rootPage->orgschema_postal_code)
-            || !empty($rootPage->orgschema_address_locality)
-            || !empty($rootPage->orgschema_address_region)
-            || !empty($rootPage->orgschema_address_country)
+            !empty($rootPage->business_street_address)
+            || !empty($rootPage->business_postal_code)
+            || !empty($rootPage->business_address_locality)
+            || !empty($rootPage->business_address_region)
+            || !empty($rootPage->business_address_country)
         ) {
             $address = new PostalAddress();
 
-            if (!empty($rootPage->orgschema_street_address)) {
-                $address->streetAddress($rootPage->orgschema_street_address);
+            if (!empty($rootPage->business_street_address)) {
+                $address->streetAddress($rootPage->business_street_address);
             }
 
-            if (!empty($rootPage->orgschema_postal_code)) {
-                $address->postalCode($rootPage->orgschema_postal_code);
+            if (!empty($rootPage->business_postal_code)) {
+                $address->postalCode($rootPage->business_postal_code);
             }
 
-            if (!empty($rootPage->orgschema_address_locality)) {
-                $address->addressLocality($rootPage->orgschema_address_locality);
+            if (!empty($rootPage->business_address_locality)) {
+                $address->addressLocality($rootPage->business_address_locality);
             }
 
-            if (!empty($rootPage->orgschema_address_region)) {
-                $address->addressRegion($rootPage->orgschema_address_region);
+            if (!empty($rootPage->business_address_region)) {
+                $address->addressRegion($rootPage->business_address_region);
             }
 
-            if (!empty($rootPage->orgschema_address_country)) {
-                $address->addressCountry($rootPage->orgschema_address_country);
+            if (!empty($rootPage->business_address_country)) {
+                $address->addressCountry($rootPage->business_address_country);
             }
 
             $org->address($address);
         }
 
-        if (!empty($rootPage->orgschema_same_as)) {
-            $sameAs = array_values(array_filter(StringUtil::deserialize($rootPage->orgschema_same_as, true)));
+        if (!empty($rootPage->business_same_as)) {
+            $sameAs = array_values(array_filter(StringUtil::deserialize($rootPage->business_same_as, true)));
 
             if ([] !== $sameAs) {
                 $org->sameAs($sameAs);
